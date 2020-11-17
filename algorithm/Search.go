c16c0d2afbadebf0614778cc5e730cb6541a224a
cuichengrui <cuichengrui@cmcm.com>
2020-11-17 22:02:14 +0800

    binSearch

---
+++ algorithm/Search.go
@@ -0,0 +1,53 @@
+package algorithm
+
+func binarySearch(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else if nums[mid] < target {
+			left = mid + 1
+		}
+	}
+	return -1
+}
+
+func binarySearchRange(nums []int, target int) []int {
+	res := []int{}
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			right = mid - 1
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else if nums[mid] < target {
+			left = mid + 1
+		}
+	}
+	if left == len(nums) || nums[left] != target {
+		res = append(res, -1)
+	} else {
+		res = append(res, left)
+	}
+	left, right = 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			left = mid + 1
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else if nums[mid] < target {
+			left = mid + 1
+		}
+	}
+	if left == len(nums) || nums[left] != target {
+		res = append(res, -1)
+	} else {
+		res = append(res, left)
+	}
+	return res
+}
