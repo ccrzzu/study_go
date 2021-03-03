26ff304fd61999354a7006a34ce259759c4effd4
cuichengrui <cuichengrui@joyme.sg>
2021-03-03 19:20:49 +0800

    add reverseKGroup

--- algorithm/linked_list/ListNodeHandle.go
+++ algorithm/linked_list/ListNodeHandle.go
@@ -234,3 +234,71 @@ func addInList(head1 *ListNode, head2 *ListNode) *ListNode {
 	}
 	return ReverseList(head.Next)
 }
+
+/**
+ *将给出的链表中的节点每\ k k 个一组翻转，返回翻转后的链表
+ *如果链表中的节点数不是\ k k 的倍数，将最后剩下的节点保持原样
+ *你不能更改节点中的值，只能更改节点本身。
+ *要求空间复杂度 \ O(1) O(1)
+ */
+func reverseKGroup(head *ListNode, k int) *ListNode {
+	// write code here
+	hair := &ListNode{Next: head}
+	pre := hair
+
+	for head != nil {
+		tail := pre
+		for i := 0; i < k; i++ {
+			tail = tail.Next
+			if tail == nil {
+				return hair.Next
+			}
+		}
+		nex := tail.Next
+		head, tail = reverseListReturnTailAndHead(head, tail)
+		pre.Next = head
+		tail.Next = nex
+		pre = tail
+		head = tail.Next
+	}
+	return hair.Next
+}
+
+func reverseListReturnTailAndHead(head, tail *ListNode) (*ListNode, *ListNode) {
+	prev := tail.Next
+	cur := head
+	for prev != tail {
+		temp := cur
+		cur = cur.Next
+		temp.Next = prev
+		prev = temp
+	}
+	return prev, head
+}
+
+func reverseKGroup2(head *ListNode, k int) *ListNode {
+	if head == nil || head.Next == nil || k == 1 {
+		return head
+	}
+	prev := &ListNode{0, nil}//哑节点，用于返回反转完成的链表
+	prev.Next = head
+	pre := prev               //前置节点
+	left, right := head, head //分别指向待翻转头尾
+	//初始化尾指针
+	for i := 0; i < k-1; i++ {
+		right = right.Next
+	}
+	count := 0 //记录步长
+	for right != nil {
+		//只有步长为k的倍数时进行翻转，可以解决剩余节点不足k的情况
+		if count%k == 0 {
+			left, right = reverseListReturnTailAndHead(left, right)
+			pre.Next = left
+		}
+		pre = pre.Next
+		left = left.Next
+		right = right.Next
+		count++
+	}
+	return prev.Next
+}
--- algorithm/tree/Tree.go
+++ algorithm/tree/Tree.go
@@ -193,6 +193,7 @@ func mergeTrees(t1 *TreeNode, t2 *TreeNode) *TreeNode {
 	return t1
 }
 
+//合并树 通过队列
 func mergeTreesByQueue(t1 *TreeNode, t2 *TreeNode) *TreeNode {
 	if t1 == nil {
 		return t2
@@ -270,8 +271,9 @@ func constructMaximumBinaryTree(nums []int) *TreeNode {
 	return root
 }
 
-//从前序和中序遍历中构造二叉树
+//重建二叉树：从前序和中序遍历中构造二叉树
 /**
+ * 递归逻辑
  * 由于同一颗子树的前序遍历和中序遍历的长度显然是相同的，
  * 所以可以根据中序遍历的根节点所在索引来得到左右子树的数量，继而得到前序遍历左右子树的索引范围
  */
@@ -292,8 +294,8 @@ func BuildTree(preorder []int, inorder []int) *TreeNode {
 	return root
 }
 
-//利用中序和后序遍历数组来构建一棵二叉树
-func buildTree2(inorder []int, postorder []int) *TreeNode {
+//重建二叉树：利用中序和后序遍历数组来构建一棵二叉树
+func BuildTree2(inorder []int, postorder []int) *TreeNode {
 	if len(postorder) == 0 {
 		return nil
 	}
@@ -305,8 +307,8 @@ func buildTree2(inorder []int, postorder []int) *TreeNode {
 		}
 	}
 	leftSize := len(inorder[:rootInorderIndex])
-	root.Left = buildTree2(inorder[:rootInorderIndex], postorder[0:leftSize])
-	root.Right = buildTree2(inorder[rootInorderIndex+1:], postorder[leftSize:len(postorder)-1])
+	root.Left = BuildTree2(inorder[:rootInorderIndex], postorder[0:leftSize])
+	root.Right = BuildTree2(inorder[rootInorderIndex+1:], postorder[leftSize:len(postorder)-1])
 	return root
 }
 
@@ -522,6 +524,7 @@ func pruneTree(root *TreeNode) *TreeNode {
 	return deal(root)
 }
 
+//树的剪枝
 func deal(node *TreeNode) *TreeNode {
 	if node == nil {
 		return nil
