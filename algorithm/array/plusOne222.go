42951047e01a6c325429f772585df9289bc3e0e6
cuichengrui <cuichengrui@joyme.sg>
2022-04-12 21:58:30 +0800

    refine

---
+++ algorithm/array/plusOne222.go
@@ -0,0 +1,26 @@
+package array
+
+import (
+	"strconv"
+	"strings"
+)
+
+
+func plusOne(nums []int)[]int{
+	var numsStr string
+	for _, item := range nums {
+		numsStr += strconv.FormatInt(item)
+	}
+	strconv.FormatInt()
+}
+
+/**
+*整数的 数组形式  num 是按照从左到右的顺序表示其数字的数组。
+
+例如，对于 num = 1321 ，数组形式是 [1,3,2,1] 。
+给定 num ，整数的 数组形式 ，和整数 k ，返回 整数 num + k 的 数组形式 。
+*/
+
+func addToArrayForm(nums []int, k int) []int {
+
+}
